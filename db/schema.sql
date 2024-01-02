1ee467251572b4bf1c7b6b2fb2293f6c9838eb5a
MarkRBishop <mrb.8@hotmail.com>
2024-01-02 13:38:47 -0600

    added schema

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+-- Create the employees_db
+CREATE DATABASE IF NOT EXISTS employees_db;
+
+--use the employees_db
+USE employees_db
+
+-- Create department table
+CREATE TABLE IF NOT EXISTS department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS role (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE IF NOT EXISTS employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
